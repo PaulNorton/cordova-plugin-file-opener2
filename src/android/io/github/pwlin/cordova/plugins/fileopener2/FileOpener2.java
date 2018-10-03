e4282a5b755665435f3b601cf25cd77e41aa2348
Paul Norton <paul@nortoncrew.com>
2018-10-03 08:15:26 -0500

    Add workaround for PDF files without ".pdf" extension to work with Adobe Acrobat on Android.

--- src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
@@ -111,7 +111,7 @@ public class FileOpener2 extends CordovaPlugin {
 						path = Uri.fromFile(file);
 					} else {
 						Context context = cordova.getActivity().getApplicationContext();
-						path = FileProvider.getUriForFile(context, cordova.getActivity().getPackageName() + ".opener.provider", file);
+						path = FileProvider.getMassagedUriForFile(context, cordova.getActivity().getPackageName() + ".opener.provider", file, contentType);
 					}
 					intent.setDataAndType(path, contentType);
 					intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
@@ -119,7 +119,7 @@ public class FileOpener2 extends CordovaPlugin {
 				} else {
 					intent = new Intent(Intent.ACTION_VIEW);
 					Context context = cordova.getActivity().getApplicationContext();
-					Uri path = FileProvider.getUriForFile(context, cordova.getActivity().getPackageName() + ".opener.provider", file);
+					Uri path = FileProvider.getMassagedUriForFile(context, cordova.getActivity().getPackageName() + ".opener.provider", file, contentType);
 					intent.setDataAndType(path, contentType);
 					intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_NO_HISTORY);
 
--- src/android/io/github/pwlin/cordova/plugins/fileopener2/FileProvider.java
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/FileProvider.java
@@ -22,8 +22,68 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 package io.github.pwlin.cordova.plugins.fileopener2;
 
+import android.os.ParcelFileDescriptor;
+import android.os.CancellationSignal;
+import android.net.Uri;
+import android.content.Context;
+import java.io.File;
+
 /*
  * http://stackoverflow.com/questions/40746144/error-with-duplicated-fileprovider-in-manifest-xml-with-cordova/41550634#41550634
  */
 public class FileProvider extends android.support.v4.content.FileProvider {
+    public static Uri getMassagedUriForFile(Context context, String authority, File file, String contentType)
+    {
+        Uri uri = null;
+
+        try {
+            uri = FileProvider.getUriForFile(context, authority, file);
+        } catch(Exception e) {
+            return null;
+        }
+
+        String path = uri.toString();
+
+        if(contentType.equals("application/pdf") && !path.endsWith(".pdf")) {
+            path = path.replace(".opener.provider", ".opener.provider/extension-added");
+            path = path + ".pdf";
+        }
+
+        return Uri.parse(path);
+    }
+
+    @Override
+    public ParcelFileDescriptor openFile(Uri uri, String mode)
+    {
+        uri = massageUri(uri);
+
+        try {
+            return super.openFile(uri, mode);
+        } catch(Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public ParcelFileDescriptor openFile(Uri uri, String mode, CancellationSignal arg2)
+    {
+        uri = massageUri(uri);
+
+        try {
+            return super.openFile(uri, mode, arg2);
+        } catch(Exception e) {
+            return null;
+        }
+    }
+
+    private Uri massageUri(Uri uri) {
+        String path = uri.toString();
+        
+        if(path.contains(".opener.provider/extension-added")) {
+            path = path.replace(".opener.provider/extension-added", ".opener.provider");
+            path = path.substring(0, path.lastIndexOf("."));
+        }
+
+        return Uri.parse(path);
+    }
 }
